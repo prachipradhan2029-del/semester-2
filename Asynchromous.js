dafd7c84469dd955778685970426478ad9ad1c0e
prachipradhan2029-del <prachi.pradhan2029@vedamsot.org>
2026-01-30 14:09:21 +0530

    Add files via upload

---
+++ Asynchromous.js
@@ -0,0 +1,16 @@
+console.log("Asynchronous programming")
+
+console.log("before");
+function fun(){
+    console.log("I am Fun")
+}
+
+setTimeout(fun,0);//delegegated to browser through we b ap1
+
+console.log("After");
+// GEC will be released and JS call stack is free 
+
+
+//---------------------async-------------------
+
+d
